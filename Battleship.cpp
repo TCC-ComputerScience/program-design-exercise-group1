c45f5347583afb31b821a941e96d36055a01fa6e
MRTCSC <56307196+MRTCSC@users.noreply.github.com>
2019-12-05 23:11:16 -0500

    Update Battleship.cpp
    I got the proper printing working, now we just need to generate the ships. Feel free to improve anything else if desired,

--- Battleship.cpp
+++ Battleship.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
 
-using namespace std;
-
 enum Marks {Hit, Miss, Water, Ship};
 enum ShipType {None, Pat, Sub, Dest, Battle, Carrier};
+enum Orientation {Horizontal, Vertical};
+
+struct battleship{
+
+    char row;
+    int  column;
+    bool location;
+
+};
 
 const int ROWS    = 11;            //default size 10
 const int COLUMNS = 11;            //default size 10
 
-// Makes a board of randomly generated values where ships are placed
-// Pre: none
-// Post: randomly puts ships on grid
-void PlaceShips(char emptyGrid[ROWS][COLUMNS]);
+using namespace std;
+
 
 // Creates a play grid based on the ship placements from PlaceShips
 // Pre: A grid with ships was created
 // Post: A grid consisting of ships and blank spaces is created but not displayed
-void InitGrid();
+void InitGrid(battleship Grid[][11]);
 
 // Prints the grid using only water, hit and miss marks
 // Pre: A play grid has been created
 // post: An updated grid is printed to the screen
-void PrintGrid();
+void PrintGrid(battleship Grid [][11]);
 
 // Tests if they hit or missed the ship
 // Pre: A valid input combination is entered
@@ -33,13 +38,11 @@ int main()
     int attempts;   // Number of guess attempts
     int maxAttempts;    // Maximum number of guess attempts
     int shipCount;      // Number of remaining ships on board
+    battleship Grid[ROWS][COLUMNS];
 
-    char emptyGrid[ROWS][COLUMNS];      // Array of random numbers for randomized ship placement
-    char boardHidden[ROWS][COLUMNS];   // Array with assigned ship locations from emptyGrid
-    char displayBoard[ROWS][COLUMNS];   // Array that displays current locations of misses and hits
-    char guessX;     // X (column) guess
-    int guessY;     // Y (row)guess
-    PrintGrid();
+
+    InitGrid(Grid);
+    PrintGrid(Grid);
     //PlaceShips();       // Makes a board of randomly generated values where ships are placed
     //InitGrid(emptyGrid);        // Creates a play grid based on the ship placements from PlaceShips
     //do    //Main game loops until finished or X guesses are completed
@@ -57,35 +60,82 @@ int main()
     return 0;
 }
 
+
+// Creates a play grid based on the ship placements from PlaceShips
+// Pre: A grid with ships was created
+// Post: A grid consisting of ships and blank spaces is created but not displayed
+void InitGrid(battleship Grid[][11]){
+
+    char letter = 'A';
+    int number = 1;
+
+
+    Grid[0][0].row = ' ';
+
+    for (int i = 1; i < ROWS; i++){      // SHOULD make top row of Grid.letter of A - J
+
+        Grid[i][0].row = letter;
+        letter++;
+    }
+
+
+    for (int i = 1; i < COLUMNS; i++){   // SHOULD make left most column of Grid.number 1 - 10
+
+        Grid[0][i].column = number;
+        number++;
+        // I changed number into an int to increment naturally
+    }
+
+    for (int i = 1; i < ROWS; i++){ //This sets every character in row in between the first column and first row waves
+
+         for (int j = 1; j < COLUMNS; j++){
+
+            Grid[i][j].row = '~';
+
+         }
+    }
+
+}
+
+
 // Prints the grid using only water, hit and miss marks
 // Pre: A play grid has been created
 // post: An updated grid is printed to the screen
-void PrintGrid(){
+void PrintGrid(battleship Grid [][11]){
 
-    char board[11][11];
-    char letter = 'A';
-    char number = '1';
+    cout << Grid[0][0].row;    // very top left index of 2D array is a space
 
-    //cout << " ";
-    board[0][0] = ' ';
-    cout << board[0][0];    // very top left index of 2D array is a space
+    cout << "  "; //Adds a space for neatness
 
-    for (int i = 1; i < ROW; i++){      // SHOULD make top row of 2D array A - J
+    for (int i = 1; i < ROWS; i++){      // SHOULD print row of 2D array A - J
 
-        board[i][0] = letter;   
-        cout << board[i][0];
+        cout << Grid[i][0].row;
         cout << " ";
-        letter++;
     }
 
     cout << '\n';
 
-    for (int i = 1; i < COLUMN; i++){   // SHOULD make left most column of 2D array 1 - 10
 
-        board[0][i] = number;
-        cout << board[0][i];
+    for (int i = 1; i < COLUMNS; i++){   // SHOULD print left most column of 2D array 1 - 10
+
+        cout << Grid[0][i].column;
+
+        if ( i != 10){
+            cout << "  ";
+        }
+
+        else { //Will only print on space when the column is 10 since the integer messes with spacing
+
+            cout << " ";
+        }
+
+        for (int j = 1; j < COLUMNS; j++){ //Prints the waves with spacing aligning with the row and column
+
+                cout << Grid[i][j].row;
+                cout << " ";
+         }
+
         cout << '\n';
-        number++;
-        // important problem: printing out 10 for the bottom left corner of an array containing chars
     }
+
 }
